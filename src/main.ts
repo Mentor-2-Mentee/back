ed1ce584082539a3b0b03982d3a007e547f8d665
정경훈 <ruderud0055@gmail.com>
2022-06-22 14:28:24 +0900

    feat : docker compose wss traefik opt 추가

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(cookieParser());
   await app.listen(configuration().apiServerPort, "0.0.0.0");
+  console.log("server start");
 }
 bootstrap();
