2bfa31cd347ed5c3c2d59345d871b1a770b15024
정경훈 <ruderud0055@gmail.com>
2022-06-30 08:42:25 +0900

    test : socket port change 80

--- src/live-chat/live-chat.gateway.ts
+++ src/live-chat/live-chat.gateway.ts
@@ -12,7 +12,7 @@ import { Server } from "http";
 import { Logger } from "@nestjs/common";
 import { Socket } from "dgram";
 
-@WebSocketGateway(8081, {
+@WebSocketGateway({
   namespace: "/live-chat",
   path: "/websocket/",
   transports: ["websocket"],
