f07f43214fdd02036ba78f9ce0b3144da82f5eff
정경훈 <ruderud0055@gmail.com>
2022-06-29 15:24:01 +0900

    fix : cname api => back

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=api.rudbeckiaz.com
+domains=back.rudbeckiaz.com
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ruderud0055@gmail.com" # Adding a valid address is strongly recommended
